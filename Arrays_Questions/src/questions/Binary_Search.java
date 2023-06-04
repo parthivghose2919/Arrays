b5331bad256a4e19acbdbdc91d43cfdd588792b9
parthiv_ghose <parthivghose2919@gmail.com>
2023-06-04 17:46:20 +0530

    Binary search code implemented with time complexity mentioned

---
+++ Arrays_Questions/src/questions/Binary_Search.java
@@ -0,0 +1,39 @@
+package questions;
+import java.util.*;
+//Binary Search
+public class Binary_Search {
+	public static int binarysearch(int arr[],int k)
+	{
+		int start=0,end=arr.length-1;
+		while (start<=end)
+		{
+			int mid=(start+end)/2;
+			//comparisions
+			if(arr[mid]==k)
+				return mid;
+			if(k>arr[mid])//right
+				start=mid+1;
+			else 
+				end=mid-1;//left
+		}
+		return -1;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc =new Scanner(System.in);
+      System.out.println("Enter the Size of the array");
+      int n=sc.nextInt();
+      int arr[]=new int[n];
+      System.out.println("Enter the numbers in the arrays");
+      for(int i=0;i<n;i++)
+      {
+    	  arr[i]=sc.nextInt();
+      }
+      System.out.println("Enter the number You want to find");
+      int k=sc.nextInt();
+      System.out.println("The index of the key is found in the position:"+binarysearch(arr,k));
+	}
+
+}
